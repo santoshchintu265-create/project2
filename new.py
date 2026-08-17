fe61fed3d2d8e0955f4b0588d10e1fd19d5b6546
santoshchintu265-create <santoshchintu265@gmail.com>
2026-08-17 12:31:44 +0530

    Initial commit

---
+++ new.py
@@ -0,0 +1,25 @@
+"""for i in range(1,101):
+    print(i)"""
+
+"""num = int(input("enter the number: "))
+
+if num % 2==0:
+    print("even num")
+else:
+    print("odd")"""
+
+
+"""for i in range(1, 101):
+    if 45 <= i <= 48:   
+        continue
+    print(i)"""
+
+"""for i in range(0,6,):
+    print("* "*i)"""
+
+
+import sys
+
+sys.stdout.write("Hello World")
+
+
